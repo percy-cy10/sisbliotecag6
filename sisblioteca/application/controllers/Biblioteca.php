36bef661079828b6190b7522f8ce0e0795fc85db
percycondori <56700873+percycondori@users.noreply.github.com>
2020-07-26 17:37:23 -0500

    ...

--- sisblioteca/application/controllers/Biblioteca.php
+++ sisblioteca/application/controllers/Biblioteca.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Biblioteca extends CI_Controller {
+class biblioteca extends CI_Controller {
 
 	public function index()
 	{
