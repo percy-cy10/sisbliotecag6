1c6ad5803a81adf2bd1ced4d47f99f10249607c7
percycondori <56700873+percycondori@users.noreply.github.com>
2020-07-26 17:41:59 -0500

    ,,,

--- sisblioteca/application/views/biblioteca/menu.php
+++ sisblioteca/application/views/biblioteca/menu.php
@@ -3,5 +3,5 @@
        <li><a href="<?php echo base_url() ?>">inicio</a></li>
        <li><a href="<?php echo base_url('Welcome/confidencial') ?>">informacion confidencial</a></li>
        <li><a href="<?php echo base_url('Welcome/confidencial2') ?>">usuarios</a></li>
-       <li><a href="<?php echo base_url('Biblioteca/salir') ?>">logout</a></li>
+       <li><a href="<?php echo base_url('biblioteca/salir') ?>">logout</a></li>
     </ul>
