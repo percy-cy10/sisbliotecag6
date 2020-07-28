f9632abfbb280a54cf2f4fae79dec3ec5a7f5bf7
percycondori <56700873+percycondori@users.noreply.github.com>
2020-07-28 14:46:37 -0500

    final login

--- sisblioteca/application/views/biblioteca/login.php
+++ sisblioteca/application/views/biblioteca/login.php
@@ -78,14 +78,10 @@
               <!-- Modal body -->
                 <div class="container">
 
-                <form name="registro" id="register-form" action="<?php echo base_url();?>ctrlusuarios/guardar" method="post" role="form" style="display: block;">
+                <form name="registro" id="register-form" action="<?php echo base_url();?>ctrlusuarios/guardar?id=0" method="post" role="form" style="display: block;">
                       <div class="card container "style="width: 100%;height: auto;">
                           <div class="card-body">
 
-<<<<<<< HEAD
-                          <form method="post"  action="<?php echo base_url();?>ctrlusuarios/guardar?id=0" >
-=======
->>>>>>> 5026b5c17a2f94db1cce3856c6e416c90a4c5eb1
                           <div class="input-group mb-3 input-group-lg">
                             <div class="input-group-prepend">
                               <span class="input-group-text">LOGIN</span>
@@ -159,13 +155,7 @@
                             </div>
                             <input type="text" class="form-control" name="usua_esadmin">
                           </div>
-                          <div class="input-group mb-3 input-group-lg">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">ESAADMIN</span>
-                              </div>
-                              <input type="text" class="form-control" name="usua_esadmin">
-                            </div>
-                          <button  class="btn btn-success" type="submit">ENVIAR</button>
+                              <button  class="btn btn-success" type="submit">ENVIAR</button>
                 </form>
    </div>
 
