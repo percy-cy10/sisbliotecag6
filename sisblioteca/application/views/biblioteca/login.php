9076cbe91b651816b0ea381d5c334d6745c82032
percycondori <56700873+percycondori@users.noreply.github.com>
2020-07-28 14:20:06 -0500

    percy

--- sisblioteca/application/views/biblioteca/login.php
+++ sisblioteca/application/views/biblioteca/login.php
@@ -121,7 +121,7 @@
 
                           <div class="input-group mb-3 input-group-lg">
                             <div class="input-group-prepend">
-                              <span class="input-group-text">NOMBRES</span>
+                              <span class="input-group-text"><i class="fas fa-user"></i></span>
                             </div>
                             <input type="text" class="form-control" name="nombres">
                           </div>
