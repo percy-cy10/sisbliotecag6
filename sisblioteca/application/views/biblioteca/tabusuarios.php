0dd40761639c6b681b7a75309d85b1ff2263209b
danysalccalagar <destroyerkiras@gmail.com>
2020-07-28 13:09:15 -0500

    practica2

--- sisblioteca/application/views/biblioteca/tabusuarios.php
+++ sisblioteca/application/views/biblioteca/tabusuarios.php
@@ -2,28 +2,38 @@
  include("estilos.php");
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div>
-      <table class="table table-bordered table-striped" >
-	    <thead class="thead-dark">
+<style>
+    
+    #div{
+       margin:0 0 0 2.5%;
+       border-radius:1%;
+  }
+
+
+</style>
+
+<div class="row">
+  <div class=" col-sm-8.2 p-3 bg-white " id="div">
+     <h1 class="text-warning text-center font-weight-bold">Lista de Usuarios</h1>
+        
+         
+
+            <table class="table table-bordered table-active">
+            <thead class="thead-dark">
+
+      <tr>
 		   <th>ID</th>
-           <th>LOGIN</th>
-           <th>PASSWORD</th>
-           <th>CODIGO</th>
+         <th>LOGIN</th>
+         <th>PASSWORD</th>
+         <th>CODIGO</th>
 		   <th>NOMBRES</th>
 		   <th>APELLIDO</th>
 		   <th>CORREO</th>
 		   <th>DIRECCION</th>
-           <th>TELEFONO</th>
-           <th>ESADMIN</th>
+         <th>TELEFONO</th>
+         <th>ESADMIN</th>
 		   <th colspan='2'>OPCIONES</th>
+      </tr>
 		</thead>
 		<tbody>
 		<?php foreach($registros as $reg): ?>
@@ -44,6 +54,5 @@
 			
 		</tbody>
 	  </table>
-	</div>
-</body>
-</html>
+     </div>
+</div>
