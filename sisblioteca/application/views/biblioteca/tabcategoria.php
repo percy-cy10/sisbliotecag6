22ff3bfe6378a40bec907741fee4811704f561fa
danysalccalagar <destroyerkiras@gmail.com>
2020-07-28 17:36:35 -0500

    dany

--- sisblioteca/application/views/biblioteca/tabcategoria.php
+++ sisblioteca/application/views/biblioteca/tabcategoria.php
@@ -8,10 +8,11 @@
        border-radius:1%;
   }
 </style>
-
-<div class="row">
+<br>
+<div class="container">
+<div class="row ">
   <div class=" col-sm-8.2 p-3 bg-white " id="div">
-     <h1 class="text-warning text-center font-weight-bold">Listas</h1>
+     <h1 class="text-warning text-center font-weight-bold">BUSQUEDA</h1>
         
          
 
@@ -29,11 +30,14 @@
                <tr>
                   <td><?php echo $reg->cate_id; ?></td>
                   <td><?php echo $reg->cate_nombre; ?></td>
-                
+                  <td>
+                     <a href="<?php echo base_url();?>ctrlusuarios/index?id=<?php echo $reg->usua_id;?>" class="btn btn-primary"><li class="fa fa-edit"></li>&nbspEditar</a>
+                  </td>
                </tr>
                <?php endforeach; ?>
 			
 		   </tbody>
 	   </table>
    </div>
+</div>
 </div>
