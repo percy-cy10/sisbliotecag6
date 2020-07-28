f30b723cd304b8350697ebae68f5b8f6b3f0a303
Julio <julitohallasi@gmail.com>
2020-07-28 10:58:39 -0500

    hallasi

--- sisblioteca/application/controllers/ctrlusuarios.php
+++ sisblioteca/application/controllers/ctrlusuarios.php
@@ -9,7 +9,28 @@ class ctrlusuarios extends CI_Controller {
 	}
 
     public function guardar(){
-		echo "hola";
+        $login= $this->input->post('login');
+        $password= $this->input->post('password');
+        $codigo= $this->input->post('codigo');
+        $nombres= $this->input->post('nombres');
+        $apellidos= $this->input->post('apellidos');
+        $correo= $this->input->post('correo');
+        $direccion= $this->input->post('direccion');
+        $telefono= $this->input->post('telefono');
+
+        $this->load->model('model_usuario');
+
+        $data = array(
+            'login'=>$login,
+            'password'=>$password,
+            'nombres'=>$nombres,
+            'apellidos'=>$apellidos,
+            'correo'=>$correo,
+            'direccion'=>$direccion,
+            'telefono'=>$telefono
+        );
+        $this->model_usuario->guardar($data);
+ 
 	}
        
 }
