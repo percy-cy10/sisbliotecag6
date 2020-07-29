0da4262bc98cea4f37cbdab6f492f8def150d40f
percycondori <56700873+percycondori@users.noreply.github.com>
2020-07-29 11:18:04 -0500

    PESTAÑA

--- sisblioteca/application/views/biblioteca/header.php
+++ sisblioteca/application/views/biblioteca/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>BIBLIOTECA EPIS</title>
+    <link rel="icon" type="image/png" href="https://bibliotecasvirtualesmundotecnologico.weebly.com/uploads/5/8/8/1/58813631/1173726.png?399">
 </head>
 <body>
