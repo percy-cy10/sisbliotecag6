fec7802199e7eae21bc861f2e16c8f85bc6b1255
danysalccalagar <destroyerkiras@gmail.com>
2020-07-29 01:09:00 -0500

    proyectos

--- sisblioteca/application/views/biblioteca/menu.php
+++ sisblioteca/application/views/biblioteca/menu.php
@@ -1,7 +1,8 @@
 <?php 
  include("estilos.php");
 
-  echo "este es la verificaion=",$this->session->userdata('veri'); 
+  $posicion=$this->session->userdata('veri'); 
+  
 ?>
 <div >
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -18,33 +19,46 @@
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url() ?>">INICIO&nbsp;&nbsp;</a>
         </li>
-        <li class="nav-item active">
+
+        <?php if($posicion==2):?>
+         <li class="nav-item active">
           <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Welcome/tabusuarios') ?>">TABUSUARIOS&nbsp;&nbsp;</a>
-        </li>
+         </li>
+       <?php endif;?>
        
+      
+      <?php if($posicion==2):?>
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Welcome/ejemplar') ?>">EJEMPLAR&nbsp;&nbsp;</a>
         </li>
-        </li>
+      <?php endif;?>
+
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Welcome/tabejemplar') ?>">TBEJEMPLAR&nbsp;&nbsp;</a>
         </li>
+     <?php if($posicion==2):?>
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Welcome/categoria') ?>">CATEGORIA&nbsp;&nbsp;</a>
         </li>
+      <?php endif;?>
+
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Welcome/tabcategoria') ?>">TBCATEGORIA&nbsp;&nbsp;</a>
         </li>
+
+        <?php if($posicion==1):?>
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('Biblioteca/mostrar') ?>">INFORMACION PERSONAL&nbsp;&nbsp;</a>
 
         </li>
+        <?php endif;?>
+
         <li class="nav-item active">
             <a class="flex-sm-fill text-sm-center nav-link " href="<?php echo base_url('biblioteca/salir') ?>">LOGOUT&nbsp;&nbsp;</a>
 
-    </li>
-
         </li>
+
+       
 
       </ul>
       
