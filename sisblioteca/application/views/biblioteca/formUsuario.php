88274c089bf775673d3e6dea418cb87ea6857c15
danysalccalagar <destroyerkiras@gmail.com>
2020-10-16 03:54:51 -0500

    trabajo

--- sisblioteca/application/views/biblioteca/formUsuario.php
+++ sisblioteca/application/views/biblioteca/formUsuario.php
@@ -15,15 +15,15 @@
              <?php
                 
                echo miForm($registros->usua_id);
-                  echo miinput('text',"usua_login",'Login:','usua_login','required',$registros->usua_login,'',4);
+                  echo miinput('text',"usua_login",'Login:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;','usua_login','required',$registros->usua_login,'',4);
                   echo miinput('text','usua_password','Password:','usua_password','required',$registros->usua_password,8,4);
-                  echo miinput('text','usua_codigo','Codigo:','usua_codigo','required',$registros->usua_codigo,6,4);
+                  echo miinput('text','usua_codigo','Codigo:&nbsp;&nbsp;&nbsp;','usua_codigo','required',$registros->usua_codigo,6,4);
                   echo miinput('text',"usua_nombres",'Nombres:','usua_nombres','required',$registros->usua_nombres,'',4);
                   echo miinput('text','usua_apellidos','Apellidos:','usua_apellidos','required',$registros->usua_apellidos,'',4);
                   echo miinput('text',"usua_direccion",'Direccion:','usua_direccion','required',$registros->usua_direccion,'',4);
-                  echo miinput('email',"usua_email",'Email:','usua_email','required',$registros->usua_email,'',4);
+                  echo miinput('email',"usua_email",'Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;','usua_email','required',$registros->usua_email,'',4);
                   echo miinput('tel',"usua_telefono",'Telefono:','usua_telefono','required',$registros->usua_telefono,9,4);
-                  echo miinput('text',"usua_esadmin",'Esadmin:','usua_esadmin','required',$registros->usua_esadmin,4,4);
+                  echo miinput('text',"usua_esadmin",'Esadmin:&nbsp;','usua_esadmin','required',$registros->usua_esadmin,4,4);
                 echo miButon('btn btn-success','Guardar');
              ?>
           </div>
