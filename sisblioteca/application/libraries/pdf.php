222f364ed01d436395b09e14b1965bc717160d7b
percycondori <56700873+percycondori@users.noreply.github.com>
2020-10-16 03:57:05 -0500

    FF

--- sisblioteca/application/libraries/pdf.php
+++ sisblioteca/application/libraries/pdf.php
@@ -1,13 +1,19 @@
-<?php
-  include(APPPATH."libraries/fpdf/fpdf.php");
 
+<?php
+
+  include(APPPATH."libraries/fpdf/fpdf.php");
+  
     class PDF extends FPDF{
         function Header()
         {
             // Logo
             //$this->Image(FCPATH.'assets/logo.jpg',10,10,35,30);
+<<<<<<< Updated upstream
             $this->Image('assets/plantilla.png',0,0,220,30);
             $this->Image('assets/pantilla.png',0,220,220,80);
+=======
+            $this->Image('assets/pantilla.jpg',0,220,220,80);
+>>>>>>> Stashed changes
             //$this->Image(FCPATH.'assets/logo1.jpg',170,10,33,33);
             $this->Image('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Logo_UNAP.png/554px-Logo_UNAP.png',6,5,20,20,'PNG');
            $this->Image('https://pbs.twimg.com/profile_images/2137261758/episunap.png',185,5,20,20,'PNG');
@@ -37,6 +43,11 @@
             $this->SetTextColor(5, 5, 250 );
             $this->Ln(4);
             $this->Cell(0,0,'"SI   PUEDES    IMAGINAR,    LO    PUEDES    PROGRAMAR"',0,0,'C');
+<<<<<<< Updated upstream
+=======
+
+            
+>>>>>>> Stashed changes
         }
         
         // Pie de página
@@ -47,8 +58,18 @@
             $this->Image('assets/julio.png',110,245,30,30);
             $this->Image('assets/manos.jpeg',100,275,20,20);
             // Posición: a 1,5 cm del final
+            $this->Image('assets/percy.png',40,245,30,30);
+            $this->Image('assets/dany.png',75,245,30,30);
+            $this->Image('assets/julio.png',110,245,30,30);
+            $this->Image('assets/manos.jpeg',100,275,30,30);
+            
+
             $this->SetY(-15);
             // Arial italic 8
+<<<<<<< Updated upstream
+=======
+            
+>>>>>>> Stashed changes
             $this->SetFont('Arial','B',15);
             // Número de página
             $this->Cell(380,15,utf8_decode('').$this->PageNo(),0,0,'C');
@@ -60,7 +81,13 @@
             $this->Cell(50,7,'ING. PERCY CONDORI Y. :jhallasia@est.unap.edu.pe',0,0,'T');
             $this->Ln(2.5);
             $this->Cell(50,7,'ING. DANY SALCA LAGAR :dsalccal@est.unap.edu.pe',0,0,'T');
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         }
+
+
 
     
         // Tabla simple
